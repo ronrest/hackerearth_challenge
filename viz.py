aaf36675ce1be350fb101fd88b12154540a4ce76
ronrest <ronny.coding@gmail.com>
2017-09-03 00:39:54 +1000

    FEAT: Add mpl_show_img()

--- viz.py
+++ viz.py
@@ -70,6 +70,18 @@ def sample_images_from_each_class(X, Y, num_per_class=5, seed=None):
     return grid_array
 
 # ==============================================================================
+#                                                                   MPL_SHOW_IMG
+# ==============================================================================
+def mpl_show_img(a, figsize=(15,10)):
+    """Given a numpy array representing an image, view it (using matplotlib)"""
+    fig, ax = plt.subplots(1, 1, figsize=figsize)
+    plt.imshow(a,  cmap="gray")     # Can actually render RGB with "gray"
+    ax.grid(False)                     # Remove gridline
+    ax.get_yaxis().set_visible(False)  # Remove axis ticks
+    ax.get_xaxis().set_visible(False)  # Remove axis ticks
+    plt.show()
+
+# ==============================================================================
 #                                                                   TRAIN_CURVES
 # ==============================================================================
 def train_curves(train, valid, saveto=None, label="Accuracy over time"):
