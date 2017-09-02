b49d78feea6b98fac46a9a87e7849dfb6ef4560c
ronrest <ronny.coding@gmail.com>
2017-09-03 00:40:41 +1000

    FEAT: Add plot_label_frequencies()

--- viz.py
+++ viz.py
@@ -101,3 +101,24 @@ def train_curves(train, valid, saveto=None, label="Accuracy over time"):
         fig.savefig(saveto)
 
 
+# ==============================================================================
+#                                                         PLOT_LABEL_FREQUENCIES
+# ==============================================================================
+def plot_label_frequencies(y, dataname="", logscale=False, saveto=None, ratio=False):
+    """ Plots the frequency of each label in the dataset."""
+    vals, freqs = np.array(np.unique(y, return_counts=True))
+    if ratio:
+        freqs = freqs / float(len(y))
+
+    fig, ax = plt.subplots(figsize=(6, 4))
+    fig.suptitle("Distribution of Labels in {} dataset".format(dataname), fontsize=15)
+    ax.bar(vals, freqs, alpha=0.5, color="#307EC7", edgecolor="b", align='center', width=0.8, lw=1)
+    ax.set_xlabel("Labels")
+    ax.set_ylabel("Frequency")
+    if logscale:
+        ax.set_yscale('log')
+    if saveto is not None:
+        fig.savefig(saveto)
+    else:
+        plt.show()
+
