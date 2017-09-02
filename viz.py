86d4649da2f6a736e33b3b9c5a25d5f81ede1bc0
ronrest <ronny.coding@gmail.com>
2017-09-03 00:41:39 +1000

    FEAT: Add plot_density_distribution()

--- viz.py
+++ viz.py
@@ -122,3 +122,21 @@ def plot_label_frequencies(y, dataname="", logscale=False, saveto=None, ratio=Fa
     else:
         plt.show()
 
+# ==============================================================================
+#                                                      PLOT_DENSITY_DISTRIBUTION
+# ==============================================================================
+def plot_density_distribution(x, saveto=None, logscale=False, dataname=""):
+    """Plots a density distribution for visualizing how values are spread out"""
+    fig, ax = plt.subplots(figsize=(6, 4))
+    sns.set_style('whitegrid')
+    sns.kdeplot(x.flatten(), bw=0.5, ax=ax)
+
+    fig.suptitle("Density disribution of {} dataset".format(dataname), fontsize=15)
+    ax.set_xlabel("Values")
+    ax.set_ylabel("Frequency")
+    if logscale:
+        ax.set_yscale('log')
+    if saveto is not None:
+        fig.savefig(saveto)
+    else:
+        plt.show()
